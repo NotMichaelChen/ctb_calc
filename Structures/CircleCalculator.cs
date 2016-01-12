bb232ec163ba2cae33a5e1dca5945d9a61193819
NotMichaelChen <mzc12345@gmail.com>
2016-01-11 20:48:57 -0500

    Created getters for private variables

--- Structures/CircleCalculator.cs
+++ Structures/CircleCalculator.cs
@@ -5,6 +5,16 @@ namespace Structures
     //Calculates a circle's radius and center given three points
     public class CircleCalculator
     {
-        
+        double center, radius
+
+        public double Center
+        {
+            get { return center; }
+        }
+
+        public double Radius
+        {
+            get { return radius; }
+        }
     }
 }
