756c3ce1f08229240e3d448d93e47ecebb8280a9
NotMichaelChen <mzc12345@gmail.com>
2016-01-16 17:24:33 -0500

    Implemeneted Bezier

--- HitObjects/BezierSlider.cs
+++ HitObjects/BezierSlider.cs
@@ -19,5 +19,22 @@ namespace HitObjects
         {
             throw new NotImplementedException();
         }
+
+        //Recursive definition of a bezier curve for any degree
+        public static Point Bezier(List<Point> controls, double t)
+        {
+            if(controls.Count == 1)
+                return controls[0];
+
+            Point result = new Point();
+
+            result.x = (1 - t) * Bezier(controls.GetRange(0, controls.Count - 1), t).x +
+                        t * Bezier(controls.GetRange(1, controls.Count - 1), t).x;
+
+            result.y = (1 - t) * Bezier(controls.GetRange(0, controls.Count - 1), t).y +
+                        t * Bezier(controls.GetRange(1, controls.Count - 1), t).y;
+
+            return result;
+        }
     }
 }
