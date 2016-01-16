ec3ab311d6c368f38a770809da1ba15531c268d7
NotMichaelChen <mzc12345@gmail.com>
2016-01-16 18:19:56 -0500

    Created BezierCurve class

---
+++ Structures/BezierCurve.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Structures
+{
+    //Represents a Bezier curve defined by a list of control points
+    //Designed specifically to work with Sliders and their control points
+    public class BezierCurve
+    {
+        
+    }
+}
