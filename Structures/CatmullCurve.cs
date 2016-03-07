edd89c36e08c4874beb5d24d8fb58cdc3e54d26d
NotMichaelChen <mzc12345@gmail.com>
2016-03-06 22:38:22 -0500

    Created method headers for CatmullCurve class

--- Structures/CatmullCurve.cs
+++ Structures/CatmullCurve.cs
@@ -6,10 +6,35 @@ namespace Structures
     public class CatmullCurve
     {
         private List<Point> controlpoints;
+        private double length;
 
-        public CatmullCurve(List<Point> points)
+        public CatmullCurve(List<Point> points, double curvelength)
         {
             controlpoints = points;
+            length = curvelength;
+
+            ReassignLastPoint();
+        }
+
+        //Gets a point along the curve
+        //t is the parameter variable that controls where along the curve the point is
+        //t goes from 0 to 1
+        public Point GetPoint(double t)
+        {
+
+        }
+
+        //Get a point that is length units along the curve
+        public Point GetPointAlong(double length)
+        {
+
+        }
+
+        //Reassigns the last control point so that it coincides with the point that
+        //is length units along the curve
+        private void ReassignLastPoint()
+        {
+
         }
     }
 }
