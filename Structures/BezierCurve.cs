58cb4832d3f37e1ba1d7782f3db8c57974838824
NotMichaelChen <mzc12345@gmail.com>
2016-05-18 16:26:29 -0400

    Removed BezierCurve.ReassignLastPoint

--- Structures/BezierCurve.cs
+++ Structures/BezierCurve.cs
@@ -8,21 +8,15 @@ namespace Structures
     public class BezierCurve
     {
         List<Point> points;
-        int sliderlength;
 
         //Constructs a Bezier curve given a list of points and a given length
         //This is necessary as a slider length may not be the actual length of the
         //defined curve
-        public BezierCurve(Point startpoint, Point[] sliderpoints, double length)
+        public BezierCurve(Point startpoint, Point[] sliderpoints)
         {
             points = new List<Point>();
             points.Add(startpoint);
             points.AddRange(sliderpoints);
-
-            //length of the curve is NOT the length of the slider
-            sliderlength = (int)Math.Round(length);
-
-            ReassignLastPoint();
         }
 
         //Wrapper method to calculate a point on the curve
@@ -46,35 +40,6 @@ namespace Structures
                         t * Bezier(controls.GetRange(1, controls.Count - 1), t).y;
 
             return result;
-        }
-
-        //Reassigns the last control point so that it coincides with the point that
-        //is length units along the curve
-        private void ReassignLastPoint()
-        {
-            //Make the number of steps either length * 5 or 1000, whichever is greater
-            double steps = sliderlength*5>1000?sliderlength*5:1000;
-            double length = 0;
-            Point prev = points[0];
-            for(int i = 1; i <= steps; i++)
-            {
-                double t = i / steps;
-                Point next = Bezier(points, t);
-                double distance = Dewlib.GetDistance(prev.x, prev.y, next.x, next.y);
-                prev = next;
-                length += distance;
-                if(length >= sliderlength)
-                {
-                    prev.x = Math.Round(prev.x);
-                    prev.y = Math.Round(prev.y);
-                    points[points.Count-1] = prev;
-                    return;
-                }
-            }
-
-            throw new InvalidOperationException("Error: Bezier curve is shorter than given length\n" +
-                                                "length: " + sliderlength + "\n" +
-                                                "distance: " + length);
         }
     }
 }
