a62332f9bcf7bbb8a9f32f11287571b3475db7f2
NotMichaelChen <mzc12345@gmail.com>
2016-05-23 18:35:33 -0400

    Implemented CatmullSlider.GetLastPoint

--- HitObjects/CatmullSlider.cs
+++ HitObjects/CatmullSlider.cs
@@ -61,5 +61,36 @@ namespace HitObjects
 
             return locations.ToArray();
         }
+        
+        private Point GetLastPoint(CatmullCurve curve, int sliderlength)
+        {
+            //Make the number of steps either length * 5 or 1000, whichever is greater
+            double steps = sliderlength*5>1000?sliderlength*5:1000;
+            //how much to increment t by with every loop
+            double increment = 1 / steps;
+            //how much along the curve we have traveled so far
+            double length = 0;
+            //where to get the next point on a given curve
+            //assign increment to get the next intended point
+            double t = increment;
+            Point prev = new Point();
+            prev.x = Int32.Parse(HitObjectParser.GetProperty(id, "x"));
+            prev.y = Int32.Parse(HitObjectParser.GetProperty(id, "y"));
+            while(t < 1)
+            {
+                Point next = curve.GetPoint(t);
+                double distance = Dewlib.GetDistance(prev.x, prev.y, next.x, next.y);
+                length += distance;
+                prev = next;
+                if(length >= sliderlength)
+                    return next;
+
+                t += increment;
+            }
+
+            //If we reached the end of the slider without accumulated sliderlength distance,
+            //just assume that the last point is the last point of the curve
+            return curve.GetPoint(1);
+        }
     }
 }
