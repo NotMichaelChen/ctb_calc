d7b33229886eb688d620eba0ffadaeabbdc1bb0f
NotMichaelChen <mzc12345@gmail.com>
2016-05-26 21:10:49 -0400

    Implemented overrided methods in CatmullSlider

--- HitObjects/CatmullSlider.cs
+++ HitObjects/CatmullSlider.cs
@@ -10,18 +10,35 @@ namespace HitObjects
 {
     public class CatmullSlider : Slider
     {
+        CatmullCurve curve;
+        
         public CatmullSlider(string id, Beatmap amap) : base(id, amap)
         {
             if(HitObjectParser.GetProperty(id, "slidertype") != "C")
                 throw new ArgumentException("Error: Hitobject provided to CatmullSlider class is not Catmull");
+            
+            Point initialcoord = new Point();
+            initialcoord.x = Int32.Parse(HitObjectParser.GetProperty(id, "x"));
+            initialcoord.y = Int32.Parse(HitObjectParser.GetProperty(id, "y"));
+            
+            curve = new CatmullCurve(initialcoord, this.controlpoints);
         }
 
         protected override int[] GetTickLocations(double tickinterval, int tickcount, int length)
         {
+            Point[] ticklocs = curve.GetTickLocations(tickinterval, tickcount, length);
+            
+            List<int> xcoords = new List<int>();
+            
+            foreach(Point i in ticklocs)
+                xcoords.Add(i.IntX());
+            
+            return xcoords.ToArray();
         }
         
         protected override Point GetLastPoint(int length)
         {
+            return curve.GetPointAlong(length);
         }
     }
 }
