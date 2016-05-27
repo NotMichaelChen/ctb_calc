abb611a28cbb17d1aaf79de3545d50d9be8b9bfe
NotMichaelChen <mzc12345@gmail.com>
2016-05-26 21:06:55 -0400

    Implemented overrided methods in PassthroughSlider

--- HitObjects/PassthroughSlider.cs
+++ HitObjects/PassthroughSlider.cs
@@ -10,18 +10,46 @@ namespace HitObjects
 {
     public class PassthroughSlider : Slider
     {
+        CircleCurve curve;
+        
         public PassthroughSlider(string id, Beatmap amap) : base(id, amap)
         {
             if(HitObjectParser.GetProperty(id, "slidertype") != "P")
                 throw new ArgumentException("Error: Hitobject provided to PassthroughSlider class is not Passthrough");
+            
+            Point initialcoord = new Point();
+            initialcoord.x = Int32.Parse(HitObjectParser.GetProperty(id, "x"));
+            initialcoord.y = Int32.Parse(HitObjectParser.GetProperty(id, "y"));
+            
+            int length = Convert.ToInt32(Math.Floor(Double.Parse(HitObjectParser.GetProperty(id, "pixelLength"))));
+            
+            curve = new CircleCurve(initialcoord, controlpoints[0], controlpoints[1], length);
         }
         
         protected override int[] GetTickLocations(double tickinterval, int tickcount, int length)
         {
+            List<int> ticks = new List<int>();
+            
+            double slidervelocity = this.GetSliderVelocity();
+            int tickrate = Int32.Parse(map.GetTag("Difficulty", "SliderTickRate"));
+            int ticklength = (int)Math.Round(slidervelocity * (100 / tickrate));
+            
+            //Will represent where the next tick is in the slider
+            int calclength = ticklength;
+            //While we haven't fallen off the end of the slider
+            while(calclength < length)
+            {
+                ticks.Add(Convert.ToInt32(curve.GetPointAlong(calclength).x));
+                //Move down the slider by a ticklength
+                calclength += ticklength;
+            }
+            
+            return ticks.ToArray();
         }
         
         protected override Point GetLastPoint(int length)
         {
+            return curve.GetPointAlong(length);
         }
     }
 }
