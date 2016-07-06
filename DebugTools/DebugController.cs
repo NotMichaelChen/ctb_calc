070d41a0de55332745964cab4a7605baa8aac9f5
NotMichaelChen <mzc12345@gmail.com>
2016-07-06 14:51:45 -0400

    Created Debug class

---
+++ DebugTools/DebugController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace DebugTools
+{
+    public class DebugController
+    {
+        string[] commands;
+        
+        public DebugController()
+        {
+            //Checks if the file exists, then loads it if it does
+            if(File.Exists("debugcommands.txt"))
+            {
+                using (StreamReader sr = new StreamReader("debugcommands.txt"))
+                {
+                    string filecontents = sr.ReadToEnd();
+                    commands = filecontents.Split(new char[] {'\n'});
+                    commands = Dewlib.TrimStringArray(commands);
+                }
+            }
+        }
+    }
+}
