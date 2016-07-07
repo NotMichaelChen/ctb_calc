eb5035a779f61431d4feaea15f8a6fc4bf01cb86
NotMichaelChen <mzc12345@gmail.com>
2016-07-07 15:00:25 -0400

    Moved difficulty calculations into separate methods

--- DiffCalc.cs
+++ DiffCalc.cs
@@ -126,96 +126,118 @@ public class DiffCalc
             velocity *= Math.Pow(circlesize+1, 1.5) / 3;
             
             double difficulty = velocity;
+
+            difficulty = this.CalculateNonmovementDifficulty(i, catcher, difficulty);
             
-            //Distance is small enough to catch without moving
-            if(Math.Abs(positions[i] - positions[i-1]) < catcher.GetCatcherSize())
-            {
-                //the distance the catcher moves, scaled as a percentage of how close the jump is to requiring movement
-                double totalpercentdistance = 0;
-                //index locating what note is the last note not needing movement to catch
-                int nonmovementindex;
-                //Makes sure that the notes aren't all in a straight line
-                int leftmost = Math.Max(positions[i], positions[i-1]);
-                int rightmost = Math.Min(positions[i], positions[i-1]);
-                //used to make sure we only get at most 10 notes
-                int nonmovecount = 0;
-                int DCcount = 0;
-                Direction curdir;
-                if(positions[i] - positions[i-1] > 0)
-                    curdir = Direction.Right;
-                else if(positions[i] - positions[i-1] < 0)
-                    curdir = Direction.Left;
-                else
-                    curdir = Direction.Stop;
-                Direction prevdir = curdir;
-                //while it's still a nonmovement jump
-                for(nonmovementindex = i; nonmovementindex > 0 && nonmovecount <= 10; nonmovementindex--)
-                {
-                    if(positions[nonmovementindex] > leftmost)
-                        leftmost = positions[nonmovementindex];
-                    else if(positions[nonmovementindex] < rightmost)
-                        rightmost = positions[nonmovementindex];
-                    
-                    if(positions[nonmovementindex] - positions[nonmovementindex-1] > 0)
-                        curdir = Direction.Right;
-                    else if(positions[nonmovementindex] - positions[nonmovementindex-1] < 0)
-                        curdir = Direction.Left;
-                    
-                    if(leftmost - rightmost > catcher.GetCatcherSize())
-                        break;
-                    if(positions[nonmovementindex] - positions[nonmovementindex-1] > catcher.GetCatcherSize())
-                        break;
-                    
-                    if(curdir != prevdir)
-                        DCcount++;
-                    
-                    prevdir = curdir;
-                    
-                    totalpercentdistance += Math.Abs(positions[nonmovementindex] - positions[nonmovementindex-1]) / (double)catcher.GetCatcherSize();
-                    nonmovecount++;
-                }
-                
-                if(times[i] != times[nonmovementindex])
-                {
-                    double pendingdiff = 100 * ((double)DCcount / (times[i] - times[nonmovementindex])) * (Math.Pow(totalpercentdistance, 3) / 100);
-                    difficulty = Math.Max(pendingdiff, difficulty);
-                }
-            }
-           
-            //Implement smarter directional change multiplier later
-            int DCindex = Array.BinarySearch(DCtimes, times[i]);
-            if(DCindex > 0)
-            {
-                int DCcount = 0;
-                double DCsum = 0;
-                for(int j = DCindex; j > 0 && DCcount <= 10; j--)
-                {
-                    DCcount++;
-                    DCsum += DCtimes[j] - DCtimes[j-1];
-                }
-                
-                //double DCmultiplier = DCcount / 3.0;
-                //Want inverse of average, so flip sum and count
-                double DCmultiplier = Math.Pow(DCcount / DCsum * 250, 2);
-                difficulty += velocity * DCmultiplier;
-            }
-            //Scale velocity based on whether the previous note was a hyper dash or not, compared to this jump
-            if(i > 1)
-            {
-                double prevvel = catcher.PercentHyper(Math.Abs(positions[i-1] - positions[i-2]) / (double)(times[i-1] - times[i-2]));
-                double thisvel = catcher.PercentHyper(Math.Abs(positions[i] - positions[i-1]) / (double)(times[i] - times[i-1]));
-                if(prevvel > 1 && thisvel <= 1)
-                {
-                    difficulty *= 2;
-                    if(i + 1 < positions.Length && Array.BinarySearch(DCtimes, times[i+1]) > 0)
-                        difficulty *= 2;
-                }
-            }
+            difficulty = this.CalculateDCDensity(i, DCtimes, velocity, difficulty);
+            
+            difficulty = this.CalculateHyperChanges(i, DCtimes, catcher, difficulty);
             
             notes.Add(new HitPoint(positions[i], times[i], difficulty));
         }
         
         return notes.ToArray();
+    }
+    
+    private double CalculateNonmovementDifficulty(int index, CatcherInfo catcher, double difficulty)
+    {
+        //Check if the last three notes can be caught without moving
+        //Prevents unnecessary checking
+        if(index > 2 && Math.Max(Math.Max(positions[index], positions[index-1]), positions[index-2]) - Math.Min(Math.Min(positions[index], positions[index-1]), positions[index-2]) < catcher.GetCatcherSize())
+        {
+            //the distance the catcher moves, scaled as a percentage of how close the jump is to requiring movement
+            double totalpercentdistance = 0;
+            //index locating what note is the last note not needing movement to catch
+            int nonmovementindex;
+            //Makes sure that the notes aren't all in a straight line
+            int leftmost = Math.Max(positions[index], positions[index-1]);
+            int rightmost = Math.Min(positions[index], positions[index-1]);
+            //used to make sure we only get at most 10 notes
+            int nonmovecount = 0;
+            int DCcount = 0;
+            Direction curdir;
+            if(positions[index] - positions[index-1] > 0)
+                curdir = Direction.Right;
+            else if(positions[index] - positions[index-1] < 0)
+                curdir = Direction.Left;
+            else
+                curdir = Direction.Stop;
+            Direction prevdir = curdir;
+            //while it's still a nonmovement jump
+            for(nonmovementindex = index; nonmovementindex > 0 && nonmovecount <= 10; nonmovementindex--)
+            {
+                if(positions[nonmovementindex] > leftmost)
+                    leftmost = positions[nonmovementindex];
+                else if(positions[nonmovementindex] < rightmost)
+                    rightmost = positions[nonmovementindex];
+                
+                if(positions[nonmovementindex] - positions[nonmovementindex-1] > 0)
+                    curdir = Direction.Right;
+                else if(positions[nonmovementindex] - positions[nonmovementindex-1] < 0)
+                    curdir = Direction.Left;
+                
+                if(leftmost - rightmost > catcher.GetCatcherSize())
+                    break;
+                if(positions[nonmovementindex] - positions[nonmovementindex-1] > catcher.GetCatcherSize())
+                    break;
+                
+                if(curdir != prevdir)
+                    DCcount++;
+                
+                prevdir = curdir;
+                
+                totalpercentdistance += Math.Abs(positions[nonmovementindex] - positions[nonmovementindex-1]) / (double)catcher.GetCatcherSize();
+                nonmovecount++;
+            }
+            
+            if(times[index] != times[nonmovementindex])
+            {
+                double pendingdiff = 100 * ((double)DCcount / (times[index] - times[nonmovementindex])) * (Math.Pow(totalpercentdistance, 3) / 100);
+                difficulty = Math.Max(pendingdiff, difficulty);
+            }
+        }
+        
+        return difficulty;
+    }
+    
+    private double CalculateDCDensity(int index, int[] DCtimes, double basevelocity, double difficulty)
+    {
+        int DCindex = Array.BinarySearch(DCtimes, times[index]);
+        if(DCindex > 0)
+        {
+            int DCcount = 0;
+            double DCsum = 0;
+            for(int j = DCindex; j > 0 && DCcount <= 10; j--)
+            {
+                DCcount++;
+                DCsum += DCtimes[j] - DCtimes[j-1];
+            }
+            
+            //double DCmultiplier = DCcount / 3.0;
+            //Want inverse of average, so flip sum and count
+            double DCmultiplier = Math.Pow(DCcount / DCsum * 250, 2);
+            difficulty += basevelocity * DCmultiplier;
+        }
+        
+        return difficulty;
+    }
+    
+    //Scale velocity based on whether the previous note was a hyper dash or not, compared to this jump
+    private double CalculateHyperChanges(int index, int[] DCtimes, CatcherInfo catcher, double difficulty)
+    {
+        if(index > 1)
+        {
+            double prevvel = catcher.PercentHyper(Math.Abs(positions[index-1] - positions[index-2]) / (double)(times[index-1] - times[index-2]));
+            double thisvel = catcher.PercentHyper(Math.Abs(positions[index] - positions[index-1]) / (double)(times[index] - times[index-1]));
+            if(prevvel > 1 && thisvel <= 1)
+            {
+                difficulty *= 2;
+                if(index + 1 < positions.Length && Array.BinarySearch(DCtimes, times[index+1]) > 0)
+                    difficulty *= 2;
+            }
+        }
+        
+        return difficulty;
     }
     
     //Each hitobject marked as a "DC" means that it requires a directional change to catch
